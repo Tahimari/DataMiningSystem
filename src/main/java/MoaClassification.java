a016049b2523b25b96d7e7c2a0a9c1ce76e3bb84
Tahimari <kamilm215@gmail.com>
2019-11-16 19:06:21 +0100

    Code optimization

--- src/main/java/MoaClassification.java
+++ src/main/java/MoaClassification.java
@@ -17,10 +17,13 @@ import java.util.Random;
 
 public class MoaClassification {
 
-    Data data;
+    // If not set then J48
     Classifier learner;
+    Data data;
     Map<String, Double> result = null;
     Boolean isTesting = false;
+    int numberSamplesCorrect = 0;
+    int numberSamples = 0;
 
     MoaClassification(Data data) {
         this.data = data;
@@ -49,8 +52,6 @@ public class MoaClassification {
                 this.learner = new DecisionStump();
                 break;
             case "4":
-                testingMenu();
-                runJ48(this.isTesting);
                 break;
             case "5":
                 Main.menu();
@@ -65,17 +66,15 @@ public class MoaClassification {
         System.out.println("[1] To run testing");
         System.out.println("[2] To run not testing");
         System.out.println("[3] Main menu");
-        this.runJ48(true);
-        Main.menu();
         switch (IOHelper.readInput()) {
             case "1":
                 this.isTesting = true;
-                result = run(true);
+                result = run();
                 Main.menu();
                 break;
             case "2":
                 this.isTesting = false;
-                run(false);
+                run();
                 Main.menu();
                 break;
             case "3":
@@ -88,27 +87,25 @@ public class MoaClassification {
 
     }
 
-    private Map<String, Double> run(boolean isTesting) {
+    private Map<String, Double> run() {
         try {
             System.out.println("Starting processing, it may take a while.");
 
+            numberSamples = 0;
+            numberSamplesCorrect = 0;
             data.prepareForUse();
-            learner.setModelContext(data.getHeader());
-            learner.prepareForUse();
 
-            int numberSamplesCorrect = 0;
-            int numberSamples = 0;
+            if (this.learner != null) {
+                learner.setModelContext(data.getHeader());
+                learner.prepareForUse();
+            }
+
             long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
 
-            while (data.hasMoreInstances() && (!data.getUseGenerator() || numberSamples < data.getNumberSamples())) {
-                Instance trainInst = data.nextInstance().getData();
-                if (isTesting) {
-                    if (learner.correctlyClassifies(trainInst)) {
-                        numberSamplesCorrect++;
-                    }
-                }
-                numberSamples++;
-                learner.trainOnInstance(trainInst);
+            if (learner != null) {
+                runMOAClassificator();
+            } else {
+                runJ48();
             }
 
             double accuracy = 100.0 * (double) numberSamplesCorrect / (double) numberSamples;
@@ -127,72 +124,64 @@ public class MoaClassification {
         return null;
     }
 
-
-    private Map<String, Double> runJ48(boolean isTesting) {
-        try {
-            System.out.println("Starting processing, it may take a while.");
-
-            data.prepareForUse();
-
-            int numberSamplesCorrect = 0;
-            int numberSamples = 0;
-            int windowSize = 100;
-            long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
-            Instances window;
-            ArrayList<String> classVals;
-            ArrayList<Attribute> attributes;
-            J48 learner = new J48();
-            SamoaToWekaInstanceConverter converter = new SamoaToWekaInstanceConverter();
-
-            while (data.hasMoreInstances() && (!data.getUseGenerator() || numberSamples < data.getNumberSamples())) {
-                classVals = new ArrayList<String>();
-                attributes = new ArrayList<Attribute>();
-                weka.core.Instance instance = converter.wekaInstance(data.nextInstance().getData());
-
-                for (int i = 1; i < instance.numAttributes(); i++) {
-                    attributes.add(new Attribute("Attribute" + (i)));
+    private void runMOAClassificator() {
+        while (data.hasMoreInstances() && (!data.getUseGenerator() || numberSamples < data.getNumberSamples())) {
+            Instance trainInst = data.nextInstance().getData();
+            if (isTesting) {
+                if (learner.correctlyClassifies(trainInst)) {
+                    numberSamplesCorrect++;
                 }
+            }
+            numberSamples++;
+            learner.trainOnInstance(trainInst);
+        }
+    }
 
-                for (int i = 1; i < instance.numClasses() + 1; i++) {
-                    classVals.add("class" + (i));
-                }
 
-                Attribute classVal = new Attribute("class", classVals);
-                attributes.add(classVal);
+    private void runJ48() throws Exception {
+        int windowSize = 100;
+        Instances window;
+        ArrayList<String> classVals;
+        ArrayList<Attribute> attributes;
+        J48 learner = new J48();
+        SamoaToWekaInstanceConverter converter = new SamoaToWekaInstanceConverter();
 
-                window = new weka.core.Instances("window", attributes, 0);
+        while (data.hasMoreInstances() && (!data.getUseGenerator() || numberSamples < data.getNumberSamples())) {
+            classVals = new ArrayList<String>();
+            attributes = new ArrayList<Attribute>();
+            weka.core.Instance instance = converter.wekaInstance(data.nextInstance().getData());
 
-                for (int i = 0; i < windowSize; i++) {
-                    if (!data.hasMoreInstances()) break;
-                    window.add(instance);
-                    instance = converter.wekaInstance(data.nextInstance().getData());
-                }
-
-                window.setClassIndex(window.numAttributes() - 1);
-                learner.buildClassifier(window);
-
-                Evaluation eval = new Evaluation(window);
-                if (isTesting) {
-                    eval.crossValidateModel(learner, window, 5, new Random(1));
-                    numberSamplesCorrect += eval.correct();
-                }
-
-                numberSamples += windowSize;
+            for (int i = 1; i < instance.numAttributes(); i++) {
+                attributes.add(new Attribute("Attribute" + (i)));
             }
 
-            double accuracy = 100.0 * (double) numberSamplesCorrect / (double) numberSamples;
-            double time = TimingUtils.nanoTimeToSeconds(TimingUtils.getNanoCPUTimeOfCurrentThread() - evaluateStartTime);
-            Map<String, Double> map = new HashMap<String, Double>();
-            map.put("accurancy", accuracy);
-            map.put("time", time);
+            for (int i = 1; i < instance.numClasses() + 1; i++) {
+                classVals.add("class" + (i));
+            }
 
-            System.out.println(numberSamples + " instances processed with " + accuracy + "% accuracy in " + time + "seconds");
+            Attribute classVal = new Attribute("class", classVals);
+            attributes.add(classVal);
 
-            return map;
-        } catch (Exception e) {
-            ConsoleColors.ansiRedErrorMessage(e);
-            Main.menu();
+            window = new weka.core.Instances("window", attributes, 0);
+
+            for (int i = 0; i < windowSize; i++) {
+                if (!data.hasMoreInstances()) {
+                    windowSize = i;
+                    break;
+                }
+                window.add(instance);
+                instance = converter.wekaInstance(data.nextInstance().getData());
+            }
+
+            window.setClassIndex(window.numAttributes() - 1);
+            learner.buildClassifier(window);
+
+            Evaluation eval = new Evaluation(window);
+            if (this.isTesting) {
+                eval.crossValidateModel(learner, window, 5, new Random(1));
+                numberSamplesCorrect += eval.correct();
+            }
+            numberSamples += windowSize;
         }
-        return null;
     }
 }
